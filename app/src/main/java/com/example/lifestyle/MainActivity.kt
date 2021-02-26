8d3faaa3824e623a7e99722bf6c9b36060909edb
taeheelee90 <taehee.lee90@gmail.com>
2021-02-26 12:55:38 +0200

    Bottom navBar added

--- app/src/main/java/com/example/lifestyle/MainActivity.kt
+++ app/src/main/java/com/example/lifestyle/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lifestyle
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var modelList = arrayListOf<Model>(
+                Model ("HOME TOUR", "https://i.pinimg.com/564x/4f/89/ab/4f89ab8891e22655177d053f495b72c5.jpg", "https://www.pinterest.co.kr/pin/119415827606871420/"),
+                Model ("Company Craftsmanship", "https://i.pinimg.com/564x/f4/74/d1/f474d10d913ff152e5a3a110abe6d6ec.jpg","https://www.pinterest.co.kr/pin/119908408817257961/"),
+                Model("The best neutral color scheme", "https://i.pinimg.com/564x/60/5b/4f/605b4f4252b7e3332a68c6a2fd3abadf.jpg", "https://www.pinterest.co.kr/pin/10203536645093510/"),
+                Model ("HOME TOUR", "https://i.pinimg.com/564x/4f/89/ab/4f89ab8891e22655177d053f495b72c5.jpg", "https://www.pinterest.co.kr/pin/119415827606871420/"),
+                Model ("Company Craftsmanship", "https://i.pinimg.com/564x/f4/74/d1/f474d10d913ff152e5a3a110abe6d6ec.jpg","https://www.pinterest.co.kr/pin/119908408817257961/"),
+                Model("The best neutral color scheme", "https://i.pinimg.com/564x/60/5b/4f/605b4f4252b7e3332a68c6a2fd3abadf.jpg", "https://www.pinterest.co.kr/pin/10203536645093510/"),
                 Model ("HOME TOUR", "https://i.pinimg.com/564x/4f/89/ab/4f89ab8891e22655177d053f495b72c5.jpg", "https://www.pinterest.co.kr/pin/119415827606871420/"),
                 Model ("Company Craftsmanship", "https://i.pinimg.com/564x/f4/74/d1/f474d10d913ff152e5a3a110abe6d6ec.jpg","https://www.pinterest.co.kr/pin/119908408817257961/"),
                 Model("The best neutral color scheme", "https://i.pinimg.com/564x/60/5b/4f/605b4f4252b7e3332a68c6a2fd3abadf.jpg", "https://www.pinterest.co.kr/pin/10203536645093510/")
@@ -41,5 +48,31 @@ class MainActivity : AppCompatActivity() {
             layoutManager = GridLayoutManager(this@MainActivity, 2)
         }
 
+        //Bottom Tab Bar Background change
+        figure_1.setOnClickListener {
+            figure_1.setBackgroundColor(Color.parseColor("#f0dca1"))
+            figure_2.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_3.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_4.setBackgroundColor(Color.parseColor("#ffffff"))
+        }
+
+        figure_2.setOnClickListener {
+            figure_1.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_2.setBackgroundColor(Color.parseColor("#f0dca1"))
+            figure_3.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_4.setBackgroundColor(Color.parseColor("#ffffff"))
+        }
+        figure_3.setOnClickListener {
+            figure_1.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_2.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_3.setBackgroundColor(Color.parseColor("#f0dca1"))
+            figure_4.setBackgroundColor(Color.parseColor("#ffffff"))
+        }
+        figure_4.setOnClickListener {
+            figure_1.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_2.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_3.setBackgroundColor(Color.parseColor("#ffffff"))
+            figure_4.setBackgroundColor(Color.parseColor("#f0dca1"))
+        }
     }
 }
